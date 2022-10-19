390136f16a0e69edc8927392cf6a8d4363058033
Tianhui Liu <Tianhui Liu@10.136.226.171>
2022-10-19 10:19:15 -0400

    Revised Class
    *[ServerThreadPool]
    	we let main thread do the listen job now

--- src/Core/ServerThreadPool.java
+++ src/Core/ServerThreadPool.java
@@ -5,35 +5,36 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ServerThreadPool {
-	private static int THREAD_NUM = 4;
-	private static boolean listen = true;
+	private static int THREAD_NUM = 4; 		// Maximum worker thread in the pool
+	private static boolean listen = true;	// Control flag for listen thread
+	private static int DEFAULT_WEL_PORT = 8080; //Assign 8080 to be default welcome port
 	
-	
-	
+	//Ask the listen thread to stop listening 
 	public static void shutdown() {
 		listen = false;
 	}
 	
 	public static void main(String[] args) throws Exception {
-		ServerSocket welcomeSocket = new ServerSocket(8080);
+		ServerSocket welcomeSocket = new ServerSocket(DEFAULT_WEL_PORT);
 		ExecutorService ServerThreadPool = Executors.newFixedThreadPool(THREAD_NUM);
-		//Listening thread
-		ServerThreadPool.submit(new Runnable() {
-		@Override
-		public void run() {
-			while(listen) {
-				try {
-					Socket connectionSocket = welcomeSocket.accept();
-					Thread workerThread = new Thread(
-							new ServerWorkerThread(connectionSocket));
-					workerThread.start();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+		
+		try {
+		while(listen) {
+			try {
+				Socket connectionSocket = welcomeSocket.accept();
+				Thread workerThread = new Thread(
+						new ServerWorkerThread(connectionSocket));
+				ServerThreadPool.execute(workerThread);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		}});
-		welcomeSocket.close();
+		}
+		}finally {
+			ServerThreadPool.shutdownNow();//
+			welcomeSocket.close();
+		}
 	}
 }
