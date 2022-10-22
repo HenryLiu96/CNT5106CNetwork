e01df2de766def7c034855b4419ffb554e8d0f6c
Tianhui Liu <Tianhui Liu@10.75.140.198>
2022-10-22 15:10:35 -0400

    Revised
    Instead of a thread, just let the pool execute a runnable

--- src/Core/ServerThreadPool.java
+++ src/Core/ServerThreadPool.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Executors;	
 
 public class ServerThreadPool {
 	private static int THREAD_NUM = 4; 		// Maximum worker thread in the pool
 	private static boolean listen = true;	// Control flag for listen thread
 	private static int DEFAULT_WEL_PORT = 8080; //Assign 8080 to be default welcome port
+	
 	
 	//Ask the listen thread to stop listening 
 	public static void shutdown() {
@@ -18,22 +18,31 @@ public class ServerThreadPool {
 	}
 	
 	public static void main(String[] args) throws Exception {
+		P2PLogger logger = new P2PLogger();
+		logger.setPrefix("ServerThreadPool");
 		ServerSocket welcomeSocket = new ServerSocket(DEFAULT_WEL_PORT);
 		ExecutorService ServerThreadPool = Executors.newFixedThreadPool(THREAD_NUM);
 		
+		logger.append("ServerThreadPool established.");
+		
 		try {
-		while(listen) {
-			try {
-				Socket connectionSocket = welcomeSocket.accept();
-				Thread workerThread = new Thread(
-						new ServerWorkerThread(connectionSocket));
-				ServerThreadPool.execute(workerThread);
-			} catch (IOException e) {
-				e.printStackTrace();
+			while(listen) {
+				try {
+					// Main thread listen for connection request to welcome socket
+					logger.append("Main socket ready to listen on welcome socket.");
+					logger.log();
+					Socket connectionSocket = welcomeSocket.accept(); 
+					// When actual connection is established, let connection socket handle
+					Runnable workerThread = new ServerWorkerThread(connectionSocket);
+					ServerThreadPool.execute(workerThread);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
-		}finally {
-			ServerThreadPool.shutdownNow();//
+		//Ensure every thread and welcome socket is closed.
+		finally {
+			ServerThreadPool.shutdownNow();
 			welcomeSocket.close();
 		}
 	}
